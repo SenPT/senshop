cc595caee78cd2d45d5f8280178e19c7f247a072
ADMINPC\Admin <phanthisen51195@gmail.com>
2019-01-13 19:53:48 +0700

    add filter status and format dât in list

---
+++ SenShop.Service/ProductCategoryService.cs
@@ -0,0 +1,72 @@
+﻿using SenShop.Data.Infrastructure;
+using SenShop.Data.Repositories;
+using SenShop.Model.Models;
+using System;
+using System.Collections.Generic;
+
+namespace SenShop.Service
+{
+    public interface IProductCategoryService
+    {
+        void Add(ProductCategory ProductCategory);
+
+        void Update(ProductCategory ProductCategory);
+
+        void Delete(int id);
+
+        IEnumerable<ProductCategory> GetAll();
+
+        IEnumerable<ProductCategory> GetAllByParentId(int parentId);
+
+        ProductCategory GetById(int id);
+
+        void Save();
+    }
+
+    public class ProductCategoryService : IProductCategoryService
+    {
+        private IProductCategoryRepository _ProductCategoryRepository;
+        private IUnitOfWork _unitOfWork;
+
+        public ProductCategoryService(IProductCategoryRepository ProductCategoryRepository, IUnitOfWork unitOfWork)
+        {
+            this._ProductCategoryRepository = ProductCategoryRepository;
+            this._unitOfWork = unitOfWork;
+        }
+
+        public void Add(ProductCategory ProductCategory)
+        {
+            _ProductCategoryRepository.Add(ProductCategory);
+        }
+
+        public void Delete(int id)
+        {
+            _ProductCategoryRepository.Delete(id);
+        }
+
+        public IEnumerable<ProductCategory> GetAll()
+        {
+            return _ProductCategoryRepository.GetAll();
+        }
+
+        public IEnumerable<ProductCategory> GetAllByParentId(int parentId)
+        {
+            return _ProductCategoryRepository.GetMulti(x => x.Status && x.ParentID == parentId);
+        }
+
+        public ProductCategory GetById(int id)
+        {
+            return _ProductCategoryRepository.GetSingleById(id);
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(ProductCategory ProductCategory)
+        {
+            _unitOfWork.Commit();
+        }
+    }
+}
