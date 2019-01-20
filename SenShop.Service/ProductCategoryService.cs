207b98514ca82f87129657317370ebda8501c577
ADMINPC\Admin <phanthisen51195@gmail.com>
2019-01-20 12:04:32 +0700

    searching for list

--- SenShop.Service/ProductCategoryService.cs
+++ SenShop.Service/ProductCategoryService.cs
@@ -16,6 +16,8 @@ namespace SenShop.Service
 
         IEnumerable<ProductCategory> GetAll();
 
+        IEnumerable<ProductCategory> GetAll(string keyword);
+
         IEnumerable<ProductCategory> GetAllByParentId(int parentId);
 
         ProductCategory GetById(int id);
@@ -47,6 +49,14 @@ namespace SenShop.Service
         public IEnumerable<ProductCategory> GetAll()
         {
             return _ProductCategoryRepository.GetAll();
+        }
+
+        public IEnumerable<ProductCategory> GetAll(string keyword)
+        {
+            if (!string.IsNullOrEmpty(keyword))
+                return _ProductCategoryRepository.GetMulti(x => x.Name.Contains(keyword) || x.Description.Contains(keyword));
+            else
+                return _ProductCategoryRepository.GetAll();
         }
 
         public IEnumerable<ProductCategory> GetAllByParentId(int parentId)
